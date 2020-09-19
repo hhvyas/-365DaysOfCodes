7d0f2d971b93da3ff6387e6ec3001b8b2eb08dc9
hhvyas <70258902+hhvyas@users.noreply.github.com>
2020-09-19 23:30:42 +0530

    Create Day13.cpp

---
+++ Day13.cpp
@@ -0,0 +1,22 @@
+/* Today I solved more 1300 problems.. */
+
+https://codeforces.com/problemset/problem/946/C
+https://ideone.com/GApom2
+
+https://codeforces.com/contest/957/problem/A
+https://ideone.com/lMAD7R
+
+https://codeforces.com/contest/1419/problem/A
+https://ideone.com/ryTdCb
+
+https://codeforces.com/contest/1419/problem/D1
+https://ideone.com/WXShpo
+
+https://codeforces.com/contest/1419/problem/B
+https://ideone.com/Txulgs
+
+https://codeforces.com/contest/1419/problem/C
+https://ideone.com/QN90lt
+
+https://codeforces.com/problemset/problem/962/A
+https://ideone.com/TZ95GU
